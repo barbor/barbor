950cc3567883872d7456251d3278af24a6cc8feb
rodrigo <rodrigo@taller.net.br>
2014-08-21 11:31:06 -0300

    Restored generic handling of fields by label.

--- tests/projects/arbor/pages/arbor_field_page.rb
+++ tests/projects/arbor/pages/arbor_field_page.rb
@@ -17,8 +17,8 @@ class ArborFieldPage
   # The below method receives the label of a field and the value that will be changed for that field
   # It already works for the following kind of fields: checkbox, radio button, text and select fields.
   def set_field field_label, field_value    
-    #field_id = self.full_content_element.html.match(/for=".+">#{field_label}/).to_s.sub('for="','').sub(">#{field_label}",'').chop 
-    field_id = self.html.match(/for=".+">#{field_label}/).to_s.sub('for="','').sub(">#{field_label}",'').chop
+    field_id = self.full_content_element.html.match(/for=".+">#{field_label}/).to_s.sub('for="','').sub(">#{field_label}",'').chop 
+    #field_id = self.html.match(/for=".+">#{field_label}/).to_s.sub('for="','').sub(">#{field_label}",'').chop
 
     if field_value == 'Marcado'
       self.execute_script("document.getElementById('" + field_id  + "').checked = true;") 
@@ -26,8 +26,8 @@ class ArborFieldPage
       self.execute_script("document.getElementById('" + field_id  + "').checked = false;") 
     else
 
-      #option_id = self.full_content_element.html.match(/value=".+">#{field_value}/).to_s.sub(/.+="/,'').sub(">#{field_value}",'').chop   
-      option_id = self.html.match(/value=".+">#{field_value}/).to_s.sub(/.+="/,'').sub(">#{field_value}",'').chop
+      option_id = self.full_content_element.html.match(/value=".+">#{field_value}/).to_s.sub(/.+="/,'').sub(">#{field_value}",'').chop   
+      #option_id = self.html.match(/value=".+">#{field_value}/).to_s.sub(/.+="/,'').sub(">#{field_value}",'').chop
 
       if option_id == ''
         self.execute_script("document.getElementById('" + field_id + "').value = '" + field_value + "';")
