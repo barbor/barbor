b8920a3938fd8ab226f7d5cea385a5fee8be1809
rodrigo <rodrigo@taller.net.br>
2014-08-21 17:36:52 -0300

    Removed debug code.

--- tests/projects/arbor/pages/arbor_ckeditor_page.rb
+++ tests/projects/arbor/pages/arbor_ckeditor_page.rb
@@ -39,8 +39,7 @@ class ArborCkeditorPage
   # end
 
   def set_ckeditor ckeditor_label, ckeditor_value
-    ckeditor_field_id = self.full_content_element.html.match(/for=".+">#{ckeditor_label}/).to_s.sub('for="','').sub(">#{ckeditor_label}",'').chop
-    puts ckeditor_field_id
+    ckeditor_field_id = self.full_content_element.html.match(/for=".+">#{ckeditor_label}/).to_s.sub('for="','').sub(">#{ckeditor_label}",'').chop    
     self.execute_script("CKEDITOR.instances['" + ckeditor_field_id + "'].setData('" + ckeditor_value + "');")
   end
 
