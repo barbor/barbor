f20113e36ab6a4f2eaee5f12c26eb45b25ce0d1e
rodrigo <rodrigo@taller.net.br>
2014-08-22 13:29:25 -0300

    Updated ArborLinkPage.

--- tests/projects/arbor/pages/arbor_link_page.rb
+++ tests/projects/arbor/pages/arbor_link_page.rb
@@ -10,7 +10,7 @@ class ArborLinkPage
   def hit_link link_text
     wait_for_ajax
 
-    link_href = self.full_content_element.html.match(/href=".+">#{link_text}/).to_s.sub('href="','').sub(">#{link_text}", '').sub('/','').sub(/".+$/, '')#.chop
+    link_href = self.full_content_element.html.match(/href=".+">#{link_text}/).to_s.sub('href="','').sub(">#{link_text}", '').sub('/','').sub(/".+$/, '').sub('"','')
 
     self.execute_script "window.location.href = '" + BASE_URL + link_href + "';"
 
