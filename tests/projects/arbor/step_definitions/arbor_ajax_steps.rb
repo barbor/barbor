6f35d74c2b5f86e65f4bdf3df718b8baff951a6f
rodrigo <rodrigo@taller.net.br>
2014-09-18 11:29:57 -0300

    Added wait for ajax ad hoc step.

--- tests/projects/arbor/step_definitions/arbor_ajax_steps.rb
+++ tests/projects/arbor/step_definitions/arbor_ajax_steps.rb
@@ -3,3 +3,7 @@
 E /^o usuário aguarda pelo carregamento do ajax$/ do
   on ArborAjaxPage do |page| page.contains_ajax end
 end
+
+E /^o usuário aguarda '(.+)' segundos$/ do |seconds|
+  on ArborAjaxPage do |page| page.sleeps_seconds seconds end
+end
